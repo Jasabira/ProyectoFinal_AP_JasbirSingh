22db832026c6249d6734c478f9a6124480094045
Jasbir Singh <jasbircoro.dev@gmail.com>
2022-06-07 21:00:46 -0300

    Acerca de
    Se agrego el componente de acerca de

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
     RedesSocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
